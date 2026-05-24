603e08d091b7a6600fdf659fea718c24f1e4ca5b
caiosid <caiosidartha8@gmail.com>
2026-05-24 11:05:35 -0300

    exemplo 3.5

---
+++ livrosjs/cap3/js/scripts3_5.js
@@ -0,0 +1,28 @@
+/*
+ * b) Elaborar um programa que leia um número e calcule sua raiz quadrada.Caso a raiz seja exata (quadrados perfeitos), informá-la, caso contrário,
+informe: ‘Não há raiz exata para ..’
+ */
+
+function calcularRaiz() {
+  var inNumero = document.getElementById("inNumero");
+  var outResposta = document.getElementById("outResposta");
+
+  var numero = Number(inNumero.value);
+
+  if (numero == 0 || isNaN(numero)) {
+    alert("Informe um número válido");
+    inNumero.focus();
+    return;
+  }
+
+  var raiz = Math.sqrt(numero);
+
+  if (raiz == Math.floor(raiz)) {
+    outResposta.textContent = `Raiz: ${raiz}`;
+  } else {
+    outResposta.textContent = `Não há raiz exata para ${numero}`;
+  }
+}
+
+var btExibir = document.getElementById("btExibir");
+btExibir.addEventListener("click", calcularRaiz);
